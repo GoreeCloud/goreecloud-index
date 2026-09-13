42a010a5baf5515da9fdc6912c2a6fd0677ffe35
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-13 08:56:05 -0500

    Normalize web action schemes locale-independently

--- app/src/main/java/com/goreecloud/index/MainActivity.kt
+++ app/src/main/java/com/goreecloud/index/MainActivity.kt
@@ -25,6 +25,7 @@ import com.goreecloud.index.provider.contacts.ContactsProvider
 import com.goreecloud.index.provider.settings.SystemSettingsProvider
 import com.goreecloud.index.ui.IndexRoot
 import com.goreecloud.index.ui.theme.GoreeCloudIndexTheme
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     private lateinit var appsProvider: InstalledAppsProvider
@@ -146,7 +147,7 @@ class MainActivity : ComponentActivity() {
 
     private fun openWeb(action: IndexAction.OpenWeb) {
         val uri = Uri.parse(action.uri)
-        val scheme = uri.scheme?.lowercase()
+        val scheme = uri.scheme?.lowercase(Locale.ROOT)
         val validWebUri = (scheme == "https" || scheme == "http") &&
             !uri.host.isNullOrBlank() &&
             uri.userInfo == null
